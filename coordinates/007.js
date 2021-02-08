c4d3dd152fdfd33812f3c82ff51789720d1e949d
Jihye Pak <jiji.pak41@gmail.comn>
2021-02-08 19:28:48 +0000

    edited template literals

--- coordinates/007.js
+++ coordinates/007.js
@@ -5,19 +5,16 @@ const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const text = document.querySelector('.text');
 
-const target_width = target.clientWidth / 2;
-const target_height = target.clientHeight / 2;
-
 
 window.addEventListener('mousemove', (e) => {
   const x = e.clientX;
   const y = e.clientY;
   text.innerHTML = `${x}, ${y}`;
-  horizontal.style.top = y + `px`;
-  vertical.style.left = x + `px`;
-  target.style.left = x - target_width + `px`;
-  target.style.top = y - target_height + `px`;
-  text.style.top = y + 10 + `px`;
-  text.style.left = x + 25 + `px`;
+  horizontal.style.top =`${y}px`;
+  vertical.style.left =`${x}px`;
+  target.style.left =`${x} px`;
+  target.style.top =`${y} px`;
+  text.style.top = `${y} px`;
+  text.style.left = `${x} px`;
 })
 
