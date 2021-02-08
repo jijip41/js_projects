6e4e9329eefb060dd65f6b1d7a8cb4ab5a0fbfa9
Jihye Pak <jiji.pak41@gmail.comn>
2021-02-08 16:33:28 +0000

    add scroll bottons

--- coordinates/index.js
+++ coordinates/index.js
@@ -1,11 +1,41 @@
 'use strict'
 
+// clientX, clientY, pageX, pageY
+
 const special = document.querySelector('.special');
 const rect = special.getBoundingClientRect();
 console.log(rect);
 special.addEventListener('click', (e) => {
   special.innerHTML = `Client: ${e.clientX}, ${e.clientY}<br/>Page:${e.pageX}, ${e.pageY}`;
   special.style.backgroundColor = "white";
+})
+
+// scroll
+
+const scroll_by = document.querySelector(".scroll_by");
+const scroll_to = document.querySelector(".scroll_to");
+const scroll_into = document.querySelector(".scroll_into");
+
+scroll_by.addEventListener('click', () => {
+  window.scrollBy({
+    top: 100,
+    left: 0,
+    behavior: 'smooth'
+  }
+  )
+})
+
+scroll_to.addEventListener('click', () => {
+  window.scrollTo({
+    top: 100,
+    left: 0,
+    behavior: 'smooth'
+  }
+  )
+})
+
+scroll_into.addEventListener('click', () => {
+  window.scroll(0, rect.top); // = special.scrollIntoView();
 })
 
 
